a68e8a9994ee4df0f795e50d3153784436733e26
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2026-02-02 11:04:53 +0100

    setup opencode

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { HomeQueryResult } from "#build/types/sanity-typegen";
+
+const homeQuery = groq`*[_type == "home"][0]{
+  _id,
+}`;
+
+const { data } = await useSanityQuery<HomeQueryResult>(homeQuery);
+</script>
+
+<template>
+  <div>
+    <h1>Data:</h1>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
   modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/scripts'
-  ]
-})
+    "@nuxt/a11y",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/scripts",
+    "@nuxtjs/sanity",
+  ],
+  runtimeConfig: {
+    sanity: {
+      token: process.env.NUXT_SANITY_TOKEN,
+    },
+  },
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: "production",
+    apiVersion: "2021-10-18",
+    perspective: "published",
+    useCdn: true,
+    typegen: {
+      enabled: true,
+      schemaTypesPath: "./studio/schemaTypes/index.ts",
+    },
+    visualEditing: {
+      token: process.env.NUXT_SANITY_TOKEN,
+      studioUrl: process.env.NUXT_SANITY_VISUAL_EDITING_STUDIO_URL,
+      stega: true,
+    },
+  },
+});
