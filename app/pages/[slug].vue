c9567c89be23656f6b3416787aa531526057e0d0
Luca Argentieri <lucaargentieri98@gmail.com>
2026-06-11 16:12:49 +0200

    commented 404 in page

--- app/pages/[slug].vue
+++ app/pages/[slug].vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-const route = useRoute()
-const { locale } = useI18n()
+const route = useRoute();
+const { locale } = useI18n();
 
 const params = computed(() => ({
   lang: locale.value,
   slug: route.params.slug as string,
-}))
+}));
 
-const { data: page } = await useSanityPage(params)
+const { data: page } = await useSanityPage(params);
 
 if (page?.value?._id) {
-  useCacheTag(`${page?.value?._id}`)
+  useCacheTag(`${page?.value?._id}`);
 }
 
-if (!page.value) {
-  throw createError({ statusCode: 404 })
-}
+// if (!page.value) {
+//   throw createError({ statusCode: 404 })
+// }
 </script>
 
 <template>
